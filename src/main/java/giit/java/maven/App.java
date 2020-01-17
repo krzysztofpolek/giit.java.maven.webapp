bd985a316e05efa2c86968292db1e20874bf2c36
krzysztofpolek <krzysztof-polek@wp.pl>
2020-01-17 09:55:45 +0100

    Add project

---
+++ src/main/java/giit/java/maven/App.java
@@ -0,0 +1,44 @@
+package giit.java.maven;
+
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.eclipse.jetty.webapp.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {
+        WebAppContext webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[] {
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new MetaInfConfiguration(),
+                new FragmentConfiguration(),
+                new EnvConfiguration(),
+                new PlusConfiguration(),
+                new JettyWebXmlConfiguration()
+        });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+
+        Server server = new Server(8080);
+        server.setHandler(webapp);
+        server.addLifeCycleListener(new AbstractLifeCycle.AbstractLifeCycleListener() {
+            @Override
+            public void lifeCycleStopped(LifeCycle event) {
+                HibernateUtil.close();
+            }
+        });
+        server.start();
+        server.join();
+    }
+
+}
---
+++ src/test/java/giit/java/maven/HelloServiceTest.java
@@ -0,0 +1,106 @@
+package giit.java.maven;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloServiceTest {
+
+    private static final String WELCOME = "Hello";
+    private static final String FALLBACK_ID_WELCOME = "Hola";
+
+    @Test
+    public void test_prepareGreeting_nullName_returnGreetingWithFallback() {
+        //given
+        LangRepository mockRepository = alwaysReturningHelloReporsitory();
+        HelloService SUT = new HelloService(mockRepository);
+
+        //when
+        String result = SUT.prepareGreeting(null, "-1");
+
+        //then
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_name_returnGreetingWithName() {
+        //given
+        LangRepository mockRepository = alwaysReturningHelloReporsitory();
+        HelloService SUT = new HelloService(mockRepository);
+        String name = "test";
+
+        //when
+        String result = SUT.prepareGreeting(name, "-1");
+
+        //then
+        assertEquals(WELCOME + " " + name + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_nonExistingLang_returnsGreetingWithFallbackLang() {
+        //given
+        LangRepository mockRepository = new LangRepository() {
+            Optional<Lang> findById(Long id) {
+                return Optional.empty();
+            }
+        };
+        HelloService SUT = new HelloService(mockRepository);
+
+        //when
+        String result = SUT.prepareGreeting(null, "-1");
+
+        //then
+        assertEquals(HelloService.FALLBACK_LANG.getWelcomeMsg() + " " + HelloService.FALLBACK_NAME + "!", result);
+
+    }
+
+    @Test
+    public void test_prepareGreeting_nullLang_returnGreetingWithFallbackIdLang() {
+        //given
+        LangRepository mockRepository = fallbackLangIdRepository();
+        HelloService SUT = new HelloService(mockRepository);
+
+        //when
+        String result = SUT.prepareGreeting(null, null);
+
+        //then
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_textLang_returnGreetingWithFallbackIdLang() {
+        //given
+        LangRepository mockRepository = fallbackLangIdRepository();
+        HelloService SUT = new HelloService(mockRepository);
+
+        //when
+        String result = SUT.prepareGreeting(null, "abc");
+
+        //then
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    private LangRepository fallbackLangIdRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Integer id) {
+                if(id.equals(HelloService.FALLBACK_LANG.getId())) {
+                    return Optional.of(new Lang(null, FALLBACK_ID_WELCOME, null));
+                }
+                return Optional.empty();
+            }
+        };
+    }
+
+    private LangRepository alwaysReturningHelloReporsitory() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Integer id) {
+                return Optional.of(new Lang(null, WELCOME, ""));
+            }
+        };
+    }
+
+}
